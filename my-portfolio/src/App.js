483fc8d2c18e9a7e6ab619900857661ef76b3306
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-05-07 09:42:43 -0400

    created react app

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { motion } from "framer-motion"
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This Will Be My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
